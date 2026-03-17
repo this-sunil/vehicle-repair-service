124c68a69803f9c51fb2152eb09973c2e53e45de
this-sunil <swarajya888@gmail.com>
2026-03-17 23:06:13 +0530

    save booking details

--- src/routes/bookRoute.js
+++ src/routes/bookRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 import upload from "../middleware/upload.js";
-import { addBookingController,getAllBookingController } from "../controller/bookController.js";
+import { addBookingController,deleteBookingController,getAllBookingController } from "../controller/bookController.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router=express.Router();
 router.post("/bookAppointment",upload.single('photo'),addBookingController);
 router.post("/bookingHistory",upload.none(),verifyToken,getAllBookingController);
 router.patch("/updateBookHistory",upload.single('photo'),verifyToken,getAllBookingController);
+router.delete("/deleteBookHistory",upload.none(),deleteBookingController);
 
 export default router;

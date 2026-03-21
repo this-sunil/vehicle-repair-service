d821498f15efabf3ef6fd822ba58f119d2814c50
this-sunil <swarajya888@gmail.com>
2026-03-21 15:47:01 +0530

    save

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,7 +1,8 @@
 import multer from "multer";
 import { v2 as cloudinary } from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
-
+import dotenv from "dotenv";
+dotenv.config();
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,

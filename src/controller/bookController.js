cf21dc5d76053ce977d411b630de054bd3640122
this-sunil <swarajya888@gmail.com>
2026-09-02 07:01:23 +0530

    fix bug

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -46,14 +46,14 @@ export const addBookingController = async (req, res) => {
         msg: "Users not found !!!",
       });
     }
-    const vehicle_photo=req.file?req.file.filename:'';
+    const photo=req.file?req.file.filename:'';
 
     const query = `INSERT INTO booking(uid,vehicle_name,registration_no,vehicle_photo,vehicle_type,slot_date,slot_time,service_name) VALUES($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *`;
     const { rows } = await pool.query(query, [
       id,
       vehicle_name,
       registration_no,
-      vehicle_photo,
+      photo,
       vehicle_type,
       slot_date,
       slot_time,

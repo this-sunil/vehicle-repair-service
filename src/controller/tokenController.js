60baba13bed261f7bcef1ba2a74f872a4022bb88
this-sunil <swarajya888@gmail.com>
2026-09-02 23:24:15 +0530

    update verify token

--- src/controller/tokenController.js
+++ src/controller/tokenController.js
@@ -1,34 +1,22 @@
 import pool from "../database/db.js";
 import { generateToken, verifyToken } from "../middleware/verifyToken.js";
 
-export const tokenController=async (req,res) => {
-    const id=req.body.id;
+export const tokenController = async (req, res) => {
     try {
-        if (!id) {
-            return res.status(400).json({
-                status: false,
-                msg: "token are required"
-            });
-        }
+        const token = await generateToken(req.user);
 
-        const isMatch = await verifyToken(refreshToken);
-
-        if (!isMatch) {
-            return res.status(401).json({
-                status: false,
-                msg: "Invalid or expired refresh token"
-            });
-        }     
-        const token=await generateToken(rows[0]);
         return res.status(200).json({
-            status:true,
-            msg:"Refresh Token Successfully !!!",
-            token:token
+            status: true,
+            msg: "Token Refreshed Successfully",
+            token
         });
+
     } catch (error) {
+        console.error(error);
+
         return res.status(500).json({
-            status:false,
-            msg:"Internal Server Error"
+            status: false,
+            msg: "Internal Server Error"
         });
     }
 };

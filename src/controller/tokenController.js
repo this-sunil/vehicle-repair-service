dd6fe4fc40c5f47cdf6df368b4d5034fd49b226c
this-sunil <swarajya888@gmail.com>
2026-09-02 23:09:21 +0530

    save token

--- src/controller/tokenController.js
+++ src/controller/tokenController.js
@@ -1,17 +1,24 @@
 import pool from "../database/db.js";
-import { generateToken } from "../middleware/verifyToken.js";
+import { generateToken, verifyToken } from "../middleware/verifyToken.js";
 
 export const tokenController=async (req,res) => {
     const id=req.body.id;
     try {
-        const query=`SELECT * FROM users WHERE id=$1`;
-        const { rows }=await pool.query(query,[id]);
-        if(rows.length===0){
-            return res.status({
-                status:false,
-                msg:"User doesn't exist !!!"
+        if (!id) {
+            return res.status(400).json({
+                status: false,
+                msg: "token are required"
             });
         }
+
+        const isMatch = await verifyToken(refreshToken);
+
+        if (!isMatch) {
+            return res.status(401).json({
+                status: false,
+                msg: "Invalid or expired refresh token"
+            });
+        }     
         const token=await generateToken(rows[0]);
         return res.status(200).json({
             status:true,
